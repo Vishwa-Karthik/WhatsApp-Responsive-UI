2902791075dd4e0cdea8d5d0804ca70746a0c6d9
Vishwa-Karthik <vishwa.prarthana@gmail.com>
2022-12-25 11:15:12 +0530

    status and call

--- lib/Tabs/myCalls_page.dart
+++ lib/Tabs/myCalls_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import '../Widgets/people_info.dart';
 
 class MyCallsPage extends StatelessWidget {
   const MyCallsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+        itemCount: info.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                info[index]['profilePic'].toString(),
+              ),
+            ),
+            title: Text(
+              info[index]['name'].toString(),
+            ),
+            subtitle: Text(
+              info[index]['time'].toString(),
+            ),
+            trailing: Icon(
+              index % 2 == 0 ? Icons.call : Icons.video_call,
+              color: Colors.teal,
+            ),
+          );
+        });
   }
 }
--- lib/Tabs/myStatus_page.dart
+++ lib/Tabs/myStatus_page.dart
@@ -1,10 +1,73 @@
 import 'package:flutter/material.dart';
 
+import '../Screens/mobile_chat_screen.dart';
+import '../Widgets/people_info.dart';
+
 class MyStatusPage extends StatelessWidget {
   const MyStatusPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: Row(
+        children: [
+          //todo my status Icon
+
+          //todo = other status visibility
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: info.length,
+              itemBuilder: ((context, index) {
+                return InkWell(
+                  onTap: () {},
+                  child: ListTile(
+                    //* Image
+                    leading: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.teal,
+                          width: 4,
+                        ),
+                      ),
+                      child: CircleAvatar(
+                        radius: 26,
+                        backgroundImage: NetworkImage(
+                          info[index]['profilePic'].toString(),
+                        ),
+                      ),
+                    ),
+
+                    //* Title Name
+                    title: Text(
+                      info[index]['name'].toString(),
+                      style: const TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+
+                    //* Message
+                    subtitle: Text(
+                      info[index]['time'].toString(),
+                      style: const TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+
+                    // //* Time
+                    // trailing: Text(
+                    //   info[index]['time'].toString(),
+                    //   style: const TextStyle(fontSize: 13, color: Colors.grey),
+                    // ),
+                  ),
+                );
+              }),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
